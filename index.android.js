606cd1caaf3ca3ebc1f90ac809bbc1477837220f
said019 <58223928+said019@users.noreply.github.com>
2020-03-23 15:47:32 -0600

    Add files via upload

---
+++ index.android.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import { AppRegistry,View,Text,StyleSheet } from 'react-native';
+
+import UsersManager from './pages/app';
+AppRegistry.registerComponent('UsersManager', () => UsersManager);
